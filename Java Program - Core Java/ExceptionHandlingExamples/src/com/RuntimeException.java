b4df6b97a0f35e934a997ae25bf12da107473483
Akash <akash300383@gmail.com>
2023-10-27 08:48:53 +0530

    done

--- Java Program - Core Java/ExceptionHandlingExamples/src/com/RuntimeException.java
+++ Java Program - Core Java/ExceptionHandlingExamples/src/com/RuntimeException.java
@@ -5,10 +5,13 @@ public class RuntimeException {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		int a=10;
-		int b=0;
+		int b=1;
+			int abc[]= {10,20,30};
 		try {
 		int res = a/b;
 		System.out.println("Result is "+res);
+			int res1 = 100/abc[5];
+			System.out.println("Result is "+res1);
 		}catch(Exception e) {
 			//System.out.println("I Take Care!");
 			//System.out.println(e.getMessage()); // display short messsage of exception 
