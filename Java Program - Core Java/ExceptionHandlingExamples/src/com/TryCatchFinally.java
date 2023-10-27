cc8ff9410a51c07535dacb9139c35b6942794dc9
Akash <akash300383@gmail.com>
2023-10-27 09:12:55 +0530

    done

---
+++ Java Program - Core Java/ExceptionHandlingExamples/src/com/TryCatchFinally.java
@@ -0,0 +1,18 @@
+package com;
+
+public class TryCatchFinally {
+
+	public static void main(String[] args) {
+	System.out.println("Hi");
+	try {
+		int res = 100/1;
+		System.out.println("res "+res);
+		System.out.println("no exception");
+	} catch (Exception e) {
+		System.out.println("catch block");
+	}finally {
+		System.out.println("finally block");
+	}
+		System.out.println("normal statement");
+	}
+}
