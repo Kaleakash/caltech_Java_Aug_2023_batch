e49f1f20b174ea79bc75e9b3ef3e7f48a749ffb9
Akash <akash300383@gmail.com>
2023-10-31 07:25:57 +0530

    done

---
+++ Java Program - Core Java/ExceptionHandlingExamples/src/com/CheckedException.java
@@ -0,0 +1,16 @@
+package com;
+
+public class CheckedException {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		System.out.println("Hello");
+		
+		//try {
+		Thread.sleep(3000);
+		//}catch(Exception e) {}
+		System.out.println("Take Tea Break...");
+	}
+
+}
