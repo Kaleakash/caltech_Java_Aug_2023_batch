97ca19cb2364555bc1fff1b621897f7a71729268
Akash <akash300383@gmail.com>
2023-11-07 08:33:58 +0530

    done

--- Java Program - Core Java/JdbcTestDemo/src/com/DemoTest.java
+++ Java Program - Core Java/JdbcTestDemo/src/com/DemoTest.java
@@ -1,11 +1,13 @@
 package com;
-
+import java.sql.*;
 public class DemoTest {
 
 	public static void main(String[] args) {
 		try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		System.out.println("Drivr Loaded successfully");
+Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/info","root","root@123");
+	System.out.println("Database connected...");
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
