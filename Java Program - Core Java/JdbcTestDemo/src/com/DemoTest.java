25047a8260b42f2c80bbd8eaba66037e73fe8c4b
Akash <akash300383@gmail.com>
2023-11-07 09:28:24 +0530

    done

--- Java Program - Core Java/JdbcTestDemo/src/com/DemoTest.java
+++ Java Program - Core Java/JdbcTestDemo/src/com/DemoTest.java
@@ -9,16 +9,25 @@ public class DemoTest {
 		try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		System.out.println("Drivr Loaded successfully");
-Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/info","root","root@123");
-System.out.println("Database connected...");	
-Statement stmt = con.createStatement();
-	int res = stmt.executeUpdate("insert into employee values(2,'Lex',34000)");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/info","root","root@123");
+		System.out.println("Database connected...");	
+		Statement stmt = con.createStatement();
+		
+		// insert Query code 
+//		int res = stmt.executeUpdate("insert into employee values(3,'Mohan',39000)");
+//		if(res>0) {
+//		System.out.println("Record inserted successfully");
+//		}
 	
-	if(res>0) {
-		System.out.println("Record inserted successfully");
-	}
-	
-	
+		// Delete Query code 
+		int res = stmt.executeUpdate("delete from employee where id=3");
+		if(res>0) {
+			System.out.println("Record deleted successfully");
+		}else {
+			System.out.println("No record present");
+		}
+		
+		
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
