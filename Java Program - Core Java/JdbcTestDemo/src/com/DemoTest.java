5c9f4c48b987e16497f6dc38824ed8dcebdfb2cb
Akash <akash300383@gmail.com>
2023-11-07 09:50:29 +0530

    done

--- Java Program - Core Java/JdbcTestDemo/src/com/DemoTest.java
+++ Java Program - Core Java/JdbcTestDemo/src/com/DemoTest.java
@@ -35,10 +35,22 @@ public class DemoTest {
 //					System.out.println("No record present");
 //				}
 		// Retrieve records 
-		ResultSet rs = stmt.executeQuery("select * from employee");
-		while(rs.next()) {
-	//System.out.println("id is "+rs.getInt(1)+" Name is  "+rs.getString(2)+" Salary is "+rs.getFloat(3));
-System.out.println("id is "+rs.getInt("id")+" Name is  "+rs.getString("name")+" Salary is "+rs.getFloat("salary"));
+//		ResultSet rs = stmt.executeQuery("select * from employee");
+//		while(rs.next()) {
+//	//System.out.println("id is "+rs.getInt(1)+" Name is  "+rs.getString(2)+" Salary is "+rs.getFloat(3));
+//System.out.println("id is "+rs.getInt("id")+" Name is  "+rs.getString("name")+" Salary is "+rs.getFloat("salary"));
+//		}
+		
+		//	it is use to insert more than one record 
+		stmt.addBatch("insert into employee values(3,'Lokesh',36000)");
+		stmt.addBatch("insert into employee values(4,'John',33000)");
+		stmt.addBatch("insert into employee values(5,'Neena',26000)");
+		stmt.addBatch("insert into employee values(6,'Teena',25000)");
+		stmt.addBatch("insert into employee values(7,'Veena',54000)");
+		
+		int res[]=stmt.executeBatch();
+		if(res.length > 0) {
+			System.out.println("All Records inserted....");
 		}
 		} catch (Exception e) {
 			System.out.println(e.toString());
@@ -47,3 +59,5 @@ System.out.println("id is "+rs.getInt("id")+" Name is  "+rs.getString("name")+" 
 	}
 
 }
+
+
