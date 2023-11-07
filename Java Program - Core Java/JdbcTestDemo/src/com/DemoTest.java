7b4cd6eb320c5c7f52f860912bdab52034b5c3ec
Akash <akash300383@gmail.com>
2023-11-07 08:56:28 +0530

    done

--- Java Program - Core Java/JdbcTestDemo/src/com/DemoTest.java
+++ Java Program - Core Java/JdbcTestDemo/src/com/DemoTest.java
@@ -2,12 +2,23 @@ package com;
 import java.sql.*;
 public class DemoTest {
 
+	
+	// create table employee(id int primary key,name varchar(20), salary float);
+	
 	public static void main(String[] args) {
 		try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		System.out.println("Drivr Loaded successfully");
 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/info","root","root@123");
-	System.out.println("Database connected...");
+System.out.println("Database connected...");	
+Statement stmt = con.createStatement();
+	int res = stmt.executeUpdate("insert into employee values(2,'Lex',34000)");
+	
+	if(res>0) {
+		System.out.println("Record inserted successfully");
+	}
+	
+	
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
