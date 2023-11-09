bf5f8bd0e574c4cee60d4af0c96e109ad8fa7905
Akash <akash300383@gmail.com>
2023-11-09 08:38:52 +0530

    done

--- Java Program - Core Java/SimpleMavenApp/src/main/java/com/DemoTest.java
+++ Java Program - Core Java/SimpleMavenApp/src/main/java/com/DemoTest.java
@@ -8,7 +8,11 @@ public class DemoTest {
 	System.out.println("Driver loaded...");
 Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/info","root","root@123");
 System.out.println("Database connected successfully");
-
+PreparedStatement pstmt = con.prepareStatement("select * from employee");
+ResultSet rs = pstmt.executeQuery();
+while(rs.next()) {
+	System.out.println("Id is "+rs.getInt(1)+" Name is "+rs.getString(2)+" Salary is "+rs.getFloat(3));
+}
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
