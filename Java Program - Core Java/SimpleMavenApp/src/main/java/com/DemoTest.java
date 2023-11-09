9a9ddff89b21165b603beabc39b201147e679972
Akash <akash300383@gmail.com>
2023-11-09 08:32:42 +0530

    done

--- Java Program - Core Java/SimpleMavenApp/src/main/java/com/DemoTest.java
+++ Java Program - Core Java/SimpleMavenApp/src/main/java/com/DemoTest.java
@@ -1,11 +1,14 @@
 package com;
-
+import java.sql.*;
 public class DemoTest {
 
 	public static void main(String[] args) {
 		try {
 	Class.forName("com.mysql.cj.jdbc.Driver");
 	System.out.println("Driver loaded...");
+Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/info","root","root@123");
+System.out.println("Database connected successfully");
+
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
