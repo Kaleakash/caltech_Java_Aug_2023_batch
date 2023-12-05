9434c7103001e6edb264d5bf1bb46090ef2980f5
akash <akash300383@gmail.com>
2023-12-05 09:59:28 +0530

    done

--- java8 and 11 features'/Java11Features/src/main/java/com/IOOperation.java
+++ java8 and 11 features'/Java11Features/src/main/java/com/IOOperation.java
@@ -8,10 +8,14 @@ public class IOOperation {
 
 	public static void main(String[] args) throws Exception{
 	// Write operation 
+//	Path filePath = Paths.get("demo.txt");
+//	String msg ="welcome to java 11 features";
+//	Files.writeString(filePath, msg);
+//	System.out.println("Data stored in file successfully");
+	// Read Operation 
 	Path filePath = Paths.get("demo.txt");
-	String msg ="welcome to java 11 features";
-	Files.writeString(filePath, msg);
-	System.out.println("Data stored in file successfully");
+	String data = Files.readString(filePath);
+	System.out.println(data);
 	}
 
 }
