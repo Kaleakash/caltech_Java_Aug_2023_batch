2d3b1b49eafddab953c24e9fd6438caa59d18bd7
akash <akash300383@gmail.com>
2023-12-19 10:24:49 +0530

    done

--- spring framework programs/SpringDIUsingXMLConfiguration/src/main/java/com/bean/Employee.java
+++ spring framework programs/SpringDIUsingXMLConfiguration/src/main/java/com/bean/Employee.java
@@ -2,6 +2,10 @@ package com.bean;
 
 public class Employee {
 
+	Employee() {
+		System.out.println("object created...");
+	}
+	
 	public void display() {
 		System.out.println("this is employee method");
 	}
