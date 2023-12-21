3889222ff0b1428adfedd307ae6b8375ae6fde11
akash <akash300383@gmail.com>
2023-12-21 10:01:46 +0530

    done

--- spring framework programs/SpringDIUsingAnnotation/src/main/java/com/main/DemoTest.java
+++ spring framework programs/SpringDIUsingAnnotation/src/main/java/com/main/DemoTest.java
@@ -1,17 +1,28 @@
 package com.main;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.bean.Address;
 import com.bean.Employee;
+import com.config.MyConfiguration;
 
 public class DemoTest {
 
 	public static void main(String[] args) {
 	
-		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+//		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+//		Employee emp1  = (Employee)ac.getBean("employee");
+//		System.out.println(emp1);  // it will call toString method 
+//		Address add = (Address)ac.getBean("address");
+//		System.out.println(add);
+		
+		AnnotationConfigApplicationContext  ac = new AnnotationConfigApplicationContext(MyConfiguration.class);  // loading config class 
 		Employee emp1  = (Employee)ac.getBean("employee");
 		System.out.println(emp1);  // it will call toString method 
+		Address add = (Address)ac.getBean("address");
+		System.out.println(add);
 	}
 
 }
