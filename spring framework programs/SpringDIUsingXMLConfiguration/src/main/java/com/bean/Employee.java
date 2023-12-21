463d4ceb4bc2d82ad5bd247acf8442baddd8fc6c
akash <akash300383@gmail.com>
2023-12-21 08:17:31 +0530

    done

--- spring framework programs/SpringDIUsingXMLConfiguration/src/main/java/com/bean/Employee.java
+++ spring framework programs/SpringDIUsingXMLConfiguration/src/main/java/com/bean/Employee.java
@@ -1,12 +1,51 @@
 package com.bean;
 
 public class Employee {
+private int id;
+private String name;
+private float salary;
 
-	Employee() {
-		System.out.println("object created...");
+	public Employee() {
+		System.out.println("object created...empty constructor");
 	}
 	
+	public Employee(int id, String name, float salary) {
+		super();
+		System.out.println("object created - parameter constructor");
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
 	public void display() {
 		System.out.println("this is employee method");
+		System.out.println("id is "+id);
+		System.out.println("name is "+name);
+		System.out.println("salary  is "+salary);
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	
 }
--- spring framework programs/SpringDIUsingXMLConfiguration/src/main/java/com/main/DemoTest.java
+++ spring framework programs/SpringDIUsingXMLConfiguration/src/main/java/com/main/DemoTest.java
@@ -16,8 +16,30 @@ public class DemoTest {
 		
 		Resource res = new ClassPathResource("beans.xml");	// load the xml file
 		BeanFactory obj = new XmlBeanFactory(res);   // get the reference of beanfactory 
-		Employee e1 = (Employee)obj.getBean("emp1");	// pull the object from a container 
-		e1.display();
+		//Employee e1 = (Employee)obj.getBean("emp1");	// pull the object from a container 
+		//e1.display();
+		
+		//Employee e2 = (Employee)obj.getBean("emp2");	// pull the object from a container 
+		//e2.display();
+		
+		//Employee e3 = (Employee)obj.getBean("emp1");	// pull the object from a container 
+		//e3.display();
+		
+		//Employee e4 = (Employee)obj.getBean("emp2");	// pull the object from a container 
+		//e4.display();
+		
+		Employee emp5 = (Employee)obj.getBean("emp1");
+		emp5.display();
+		
+		Employee emp6 = (Employee)obj.getBean("emp2");
+		emp6.display();
+		
+		Employee emp7 = (Employee)obj.getBean("emp3");
+		emp7.display();
+		
+		Employee emp8 = (Employee)obj.getBean("emp4");
+		emp8.display();
+		
 		
 	}
 
