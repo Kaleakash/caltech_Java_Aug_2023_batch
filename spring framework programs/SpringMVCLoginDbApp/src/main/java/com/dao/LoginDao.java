9c0b8a8fe71cf3eb4d0d460f0859fa198cbb52f9
akash <akash300383@gmail.com>
2024-01-04 10:16:47 +0530

    done

--- spring framework programs/SpringMVCLoginDbApp/src/main/java/com/dao/LoginDao.java
+++ spring framework programs/SpringMVCLoginDbApp/src/main/java/com/dao/LoginDao.java
@@ -1,5 +1,43 @@
 package com.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.bean.Login;
+
+@Repository
 public class LoginDao {
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public int signIn(Login login) {
+		try {
+List<Map<String, Object>> list=	
+jdbcTemplate.queryForList("select * from login where emailid =? and password =?", login.getEmailid(),login.getPassword());	
+		return list.size();
+		} catch (Exception e) {
+			System.err.println(e);
+			return 0;
+		}
+	}
+	
+	public int signUp(Login login) {
+		try {
+	return jdbcTemplate.update("insert into login values(?,?)", login.getEmailid(),login.getPassword());		
+		} catch (Exception e) {
+			System.err.println(e);
+			return 0;
+		}
+	}
 }
+
+
+
+
+
+
