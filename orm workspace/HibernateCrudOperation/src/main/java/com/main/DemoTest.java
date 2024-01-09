a8507719dd6595bd826901d8ba04db88e08b9121
akash <akash300383@gmail.com>
2024-01-09 08:41:39 +0530

    done

--- orm workspace/HibernateCrudOperation/src/main/java/com/main/DemoTest.java
+++ orm workspace/HibernateCrudOperation/src/main/java/com/main/DemoTest.java
@@ -73,24 +73,58 @@ public class DemoTest {
 //	}
 	Scanner sc = new Scanner(System.in);
 	
-	// Retrieve more than one record 
-	// DAO layer code 
-	//TypedQuery<Employee> qry = session.createQuery("select emp from Employee emp");
-	// Static value 
-	//TypedQuery<Employee> qry = session.createQuery("select emp from Employee emp where emp.id=101");
-	//dynamic value usign label parameter 
-	System.out.println("Enter the id");
-	int id = sc.nextInt();
-	TypedQuery<Employee> qry = session.createQuery("select emp from Employee emp where emp.id=:empid");
-	qry.setParameter("empid", id);
-	List<Employee> listOfEmp = qry.getResultList();  // directly converted to list of object 
+//	// Retrieve more than one record 
+//	// DAO layer code 
+//	//TypedQuery<Employee> qry = session.createQuery("select emp from Employee emp");
+//	// Static value 
+//	//TypedQuery<Employee> qry = session.createQuery("select emp from Employee emp where emp.id=101");
+//	//dynamic value usign label parameter 
+//	System.out.println("Enter the id");
+//	int id = sc.nextInt();
+//	TypedQuery<Employee> qry = session.createQuery("select emp from Employee emp where emp.id=:empid");
+//	qry.setParameter("empid", id);
+//	List<Employee> listOfEmp = qry.getResultList();  // directly converted to list of object 
+//	
+//	// View layer code 
+//	System.out.println("Number of records "+listOfEmp.size());
+//	Iterator<Employee> li = listOfEmp.iterator();
+//	while(li.hasNext()) {
+//		Employee e = li.next();
+//		System.out.println(e);
+//	}
 	
-	// View layer code 
-	System.out.println("Number of records "+listOfEmp.size());
-	Iterator<Employee> li = listOfEmp.iterator();
-	while(li.hasNext()) {
-		Employee e = li.next();
-		System.out.println(e);
-	}
-	}
+//	// retreive partial object ie id 
+//	
+//	TypedQuery<Integer> qry = session.createQuery("select emp.id from Employee emp");
+//	List<Integer> listOfIds = qry.getResultList();
+//	Iterator<Integer> li = listOfIds.iterator();
+//	while(li.hasNext()) {
+//		int id  = li.next();
+//		System.out.println("id is "+id);
+//	}
+//	
+
+	// retreive partial object ie name 
+//	
+//		TypedQuery<String> qry = session.createQuery("select emp.name from Employee emp");
+//		List<String> listOfNames = qry.getResultList();
+//		Iterator<String> li = listOfNames.iterator();
+//		while(li.hasNext()) {
+//			String name  = li.next();
+//			System.out.println("name is "+name);
+//		}
+	
+	// retreive partial object is name and salary 
+	// join for we need to do object[] type casting 
+		TypedQuery<Object[]> qry = session.createQuery("select emp.name,emp.salary from Employee emp");
+		List<Object[]> listOfNameAndSalary = qry.getResultList();
+		Iterator<Object[]> li = listOfNameAndSalary.iterator();
+		while(li.hasNext()) {
+			Object obj[]=li.next();
+			System.out.println("name is "+obj[0]+" Salary is "+obj[1]);
+		}
+		
+		
+	
+		}
 }
