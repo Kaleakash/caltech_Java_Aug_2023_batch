f537f29ae38eb9dd4b2674b5bdfca34d59887312
akash <akash300383@gmail.com>
2024-01-09 10:07:04 +0530

    done

---
+++ orm workspace/SpringMVCWithHibernate/src/main/java/com/bean/Product.java
@@ -0,0 +1,38 @@
+package com.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)  // auto_generate value for pid
+private int pid;
+private String pname;
+private float price;
+public int getPid() {
+	return pid;
+}
+public void setPid(int pid) {
+	this.pid = pid;
+}
+public String getPname() {
+	return pname;
+}
+public void setPname(String pname) {
+	this.pname = pname;
+}
+public float getPrice() {
+	return price;
+}
+public void setPrice(float price) {
+	this.price = price;
+}
+@Override
+public String toString() {
+	return "Product [pid=" + pid + ", pname=" + pname + ", price=" + price + "]";
+}
+
+}
