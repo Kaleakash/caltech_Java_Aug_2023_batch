f9b585cf918ab6e1a9d7ce77bed3e48ba88a749b
akash <akash300383@gmail.com>
2024-01-12 07:49:37 +0530

    done

--- orm workspace/JPACrudOperation/src/main/java/com/main/DemoTest.java
+++ orm workspace/JPACrudOperation/src/main/java/com/main/DemoTest.java
@@ -5,25 +5,52 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.hibernate.Session;
+
 import com.bean.Employee;
 
 public class DemoTest {
 
 	public static void main(String[] args) {
 	// using persistence unit name part of persistence.xml file we get database details and mapping file details 	
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPACrudOperation");	
-	EntityManager manager = emf.createEntityManager();	// like SessionFactory 
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPACrudOperation");	// SessionFactory 
+	EntityManager manager = emf.createEntityManager();	// like Session
 	EntityTransaction tran = manager.getTransaction();		// like Transaction 
 	
-	Employee emp = new Employee();
-	emp.setId(104);
-	emp.setName("Ajay");
-	emp.setSalary(38000);
+	// insert Query 
+//	Employee emp = new Employee();
+//	emp.setId(104);
+//	emp.setName("Ajay");
+//	emp.setSalary(38000);
+//	
+//	tran.begin();
+//		manager.persist(emp);    // like save 
+//	tran.commit();
+//	System.out.println("Record inserted successfully");
 	
-	tran.begin();
-		manager.persist(emp);    // like save 
-	tran.commit();
-	System.out.println("Record inserted successfully");
+//	// Delete Query 
+//	Employee emp = manager.find(Employee.class, 101);
+//	if(emp==null) {
+//		System.out.println("Record not present");
+//	}else {
+//		tran.begin();
+//				manager.remove(emp);     // like delete 
+//		tran.commit();
+//		System.out.println("Record deleted successfully");
+//	}
+	
+	// Update Query 
+		Employee emp = manager.find(Employee.class, 102);
+		if(emp==null) {
+			System.out.println("Record not present");
+		}else {
+			tran.begin();
+				emp.setSalary(45000);
+				manager.merge(emp);			// like update 
+			tran.commit();
+			System.out.println("Record updated successfully");
+		}
+		
 	}
 
 }
