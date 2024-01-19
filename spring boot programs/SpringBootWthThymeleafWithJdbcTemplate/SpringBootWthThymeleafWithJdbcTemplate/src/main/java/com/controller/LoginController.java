c2942d3efeff01e5d55a43367b93037765bfa720
akash <akash300383@gmail.com>
2024-01-19 09:02:51 +0530

    done

--- spring boot programs/SpringBootWthThymeleafWithJdbcTemplate/SpringBootWthThymeleafWithJdbcTemplate/src/main/java/com/controller/LoginController.java
+++ spring boot programs/SpringBootWthThymeleafWithJdbcTemplate/SpringBootWthThymeleafWithJdbcTemplate/src/main/java/com/controller/LoginController.java
@@ -2,9 +2,11 @@ package com.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.bean.Login;
 import com.service.LoginService;
 
 @Controller
@@ -12,10 +14,20 @@ public class LoginController {
 
 	@Autowired
 	LoginService loginService;
+	// http://localhost:8080/
 	
 	@RequestMapping(value = "/",method = RequestMethod.GET)
-	public String login() {
-		System.out.println("I Came Here");
+	public String login(Model model,Login login) {  // DI for Model API part of spring boot
+		System.out.println("I Came Here");	// model is like ModelAndView class reference 
+		model.addAttribute("ll", login);   // store login object in model scope. 
 		return "login";
+	}
+	
+	@RequestMapping(value = "/checkLogin",method = RequestMethod.POST)
+	public String signIn(Model model,Login login) {  // DI for Model API part of spring boot
+		System.out.println(login.getEmailid()+" "+login.getPassword());
+		model.addAttribute("ll", login);   // store login object in model scope. 
+		String result = loginService.signIn(login);
+		return result;
 	}
 }
