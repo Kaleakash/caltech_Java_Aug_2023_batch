6669cd3d915c5239ee39f4c0959718c08d0e2dc2
akash <akash300383@gmail.com>
2024-01-25 07:55:22 +0530

    done

--- spring boot programs/ProductManagementRestAPI/ProductManagementRestAPI/src/main/java/com/service/ProductService.java
+++ spring boot programs/ProductManagementRestAPI/ProductManagementRestAPI/src/main/java/com/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,41 @@ public class ProductService {
 	
 	public List<Product> findAllProductInfo() {
 		return productRepository.findAll();
+	}	
+	public String deleteProduct(int pid) {
+		
+//		productRepository.deleteById(pid);
+//		return "Record deleted ";
+		Optional<Product> op=productRepository.findById(pid);		// find record by id 
+		if(op.isPresent()) {
+//		Product p	= op.get();							// if present get that record 
+//		productRepository.delete(p);					// delete that record 
+			productRepository.deleteById(pid);		
+		return "Product deleted successfully";
+		}else {
+			return "Product id not present";
+		}
+	}
+	
+	public String updateProduct(Product prodct) {
+		Optional<Product> op=productRepository.findById(prodct.getPid());		// find record by id 
+		if(op.isPresent()) {
+		Product p	= op.get();							// if present get that record 
+		p.setPrice(prodct.getPrice());    // p retrieve from database base upon id and set new price
+			productRepository.saveAndFlush(p);	
+		return "Product updated successfully";
+		}else {
+			return "Product id not present";
+		}
+	}
+
+	public String findProduct(int pid) {
+		Optional<Product> op=productRepository.findById(pid);		// find record by id 
+		if(op.isPresent()) {
+		Product p	= op.get();							// if present get that record 
+		return p.toString();
+		}else {
+			return "Product id not present";
+		}
 	}
 }
