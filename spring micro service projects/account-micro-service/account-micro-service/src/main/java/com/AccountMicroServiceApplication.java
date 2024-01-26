6fdfc6484f62788e9940d8c96c99833658823ac0
akash <akash300383@gmail.com>
2024-01-26 10:14:36 +0530

    done

--- spring micro service projects/account-micro-service/account-micro-service/src/main/java/com/AccountMicroServiceApplication.java
+++ spring micro service projects/account-micro-service/account-micro-service/src/main/java/com/AccountMicroServiceApplication.java
@@ -2,12 +2,19 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EntityScan(basePackages = "com.bean")
+@EnableJpaRepositories(basePackages = "com.repository")
+@EnableDiscoveryClient
 public class AccountMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMicroServiceApplication.class, args);
+		System.err.println("Account Micro Service up on port number 8383!");
 	}
 
 }
