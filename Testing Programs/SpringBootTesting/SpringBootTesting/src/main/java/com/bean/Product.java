b8539abad863e4e1eb5e606517d2e5c431a2a8e9
akash <akash300383@gmail.com>
2024-02-02 07:58:34 +0530

    done

---
+++ Testing Programs/SpringBootTesting/SpringBootTesting/src/main/java/com/bean/Product.java
@@ -0,0 +1,41 @@
+package com.bean;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int pid;
+private String pname;
+private float price;
+private int qty;
+public int getPid() {
+	return pid;
+}
+public void setPid(int pid) {
+	this.pid = pid;
+}
+public String getPname() {
+	return pname;
+}
+public void setPname(String pname) {
+	this.pname = pname;
+}
+public float getPrice() {
+	return price;
+}
+public void setPrice(float price) {
+	this.price = price;
+}
+public int getQty() {
+	return qty;
+}
+public void setQty(int qty) {
+	this.qty = qty;
+}
+
+}
---
+++ Testing Programs/SpringBootTesting/SpringBootTesting/src/main/java/com/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bean.Product;
+import com.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	ProductRepository productRepository;
+	
+	public List<Product> findAllProduct() {
+		return productRepository.findAll();
+	}
+}
