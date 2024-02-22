07c0d3f25d538602820284c6d9646b892312f92b
akash <akash300383@gmail.com>
2024-02-22 10:13:30 +0530

    done

--- course 3 e-shopping app/backend/Login-micro-service-app/Login-micro-service-app/src/main/java/com/LoginMicroServiceAppApplication.java
+++ course 3 e-shopping app/backend/Login-micro-service-app/Login-micro-service-app/src/main/java/com/LoginMicroServiceAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.entity.Login;
@@ -18,7 +18,7 @@ import jakarta.annotation.PostConstruct;
 @SpringBootApplication(scanBasePackages = "com")
 @EntityScan(basePackages = "com.entity")
 @EnableJpaRepositories(basePackages = "com.repository")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class LoginMicroServiceAppApplication {
 
 	@Autowired
